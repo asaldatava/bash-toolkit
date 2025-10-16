697210ee1280dab67f4d4e6fed6cdd2ca29557be
asa <ksenia.soldatova@gmail.com>
2025-10-16 23:08:31 +0200

    Add debug & convert variables into global; add handling errors

--- lib/functions.sh
+++ lib/functions.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
+set -x
 
 source config/file.env
 
-function backup() {
-    rotate_backups
-    calc_checksum
-}
+CURRENT_DATE=$(date '+%Y%m%d%H%M%S')
+LOG_NAME_INFO=logs/backup_${CURRENT_DATE}_INFO.log
+LOG_NAME_ERROR=logs/backup_${CURRENT_DATE}_ERROR.log
 
-function log() {
-  if [[ ! -f "$PATH_TO_FILE" ]]; then
-    echo "ERROR: $PATH_TO_FILE does no exist"
-    exit 1
-  fi
-  grep -m "$MAX_SEARCH" -F "$PATTERN" "$PATH_TO_FILE"
-  exit 0
+exec 2>$LOG_NAME_ERROR
+exec >$LOG_NAME_INFO
+
+function backup() {
+   if ! command -v backup &> /dev/null; then
+        error_exit "Required command not found" 2
+   fi
+   if ! rotate_backups; then
+    error_exit "rotate backups failed"
+   fi
+    calc_checksum
+    clean_old_backups
 }
 
 function rotate_backups() {
-    CURRENT_DATE=$(date '+%Y%m%d%H%M%S')
-    ARCHIVE_NAME=backup_${CURRENT_DATE}.tar.gz
-    LOG_NAME_INFO=logs/backup_${CURRENT_DATE}_INFO.log
-    LOG_NAME_ERROR=logs/backup_${CURRENT_DATE}_ERROR.log
+  if [ "$#" -ne 4 ]; then
+    show_usage
+  fi
+  echo "Argument 1: $1"
+  echo "Argument 2: $2"
+  echo "Argument 3: $3"
+  echo "Argument 4: $4"
+
+ARCHIVE_NAME=backup_${CURRENT_DATE}.tar.gz
+
 if [[ ! -d "$DESTINATION_DIR" ]]; then
   mkdir -p "$DESTINATION_DIR"
   echo "INFO: creating a ${DESTINATION_DIR} directory"
@@ -33,29 +44,50 @@ else
   echo "INFO: creating ${ARCHIVE_NAME} in ${DESTINATION_DIR}"
   exit 0
 fi
+  tar -cvzf $DESTINATION_DIR/${ARCHIVE_NAME} $SOURCE 2>&1
+}
 
-  tar -cvzf $DESTINATION_DIR/${ARCHIVE_NAME} $SOURCE > ${LOG_NAME_INFO} 2>${LOG_NAME_ERROR}
-# Clean up old backups
-BACKUPS=($(ls -dt $DESTINATION_DIR/backup_*))
-BACKUP_COUNT=${#BACKUPS[@]}
-if [ "$BACKUP_COUNT" -gt "$MAX_BACKUPS" ]; then
-  for (( i="$MAX_BACKUPS"; i<$BACKUP_COUNT; i++ ))
-  do
-    rm -rf "${BACKUPS[$i]}"
-    echo "Old backup deleted: ${BACKUPS[$i]}"
-  done
-fi
-exit 0
+function clean_old_backups() {
+    BACKUPS=($(ls -dt $DESTINATION_DIR/backup_*))
+    BACKUP_COUNT=${#BACKUPS[@]}
+    if [ "$BACKUP_COUNT" -gt "$MAX_BACKUPS" ]; then
+      for (( i="$MAX_BACKUPS"; i<$BACKUP_COUNT; i++ ))
+      do
+        rm -rf "${BACKUPS[$i]}"
+        echo "Old backup deleted: ${BACKUPS[$i]}"
+      done
+    fi
+    if [ $? -ne 0 ]; then
+      echo "Error: Failed to clean old backup"
+      exit 1
+    fi
 }
 
 function calc_checksum() {
     echo -n ${ARCHIVE_NAME} | sha256sum > checksum.txt
 }
 
-#function log_info() {
-#
-#}
-#
-#function log_error() {
-#
-#}
+function error_exit() {
+    log_error "$1"
+    exit "${2:-1}"
+}
+
+function log_error() {
+    local timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+    echo "$timestamp ERROR: $1" >> "$LOG_NAME_ERROR"
+    echo "ERROR: $1" >&2
+}
+
+function log() {
+  if [[ ! -f "$PATH_TO_FILE" ]]; then
+    echo "ERROR: $PATH_TO_FILE does no exist"
+    exit 1
+  fi
+  grep -m "$MAX_SEARCH" -F "$PATTERN" "$PATH_TO_FILE"  2>&1
+  exit 0
+}
+
+  function show_usage() {
+    echo "Usage: $0 [command] [source] [destination] [backups-number]"
+    exit 1
+  }
--- toolkit.sh
+++ toolkit.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
+set -x
 
 source lib/functions.sh
 
 backup
-log
+#log
 
 case "$1" in
-    "") ;;
-    backup) "$@"; exit;;
-    log) "$@"; exit;;
-    *) log "Unknown function: $1()"; exit 2;;
+  backup)    # this is the name of the argument triggering the function - could be different, for instance f1
+    backup   # this is the name of the function to run
+    shift
+    ;;
+
+  log)    # this is the name of the argument triggering the function - could be different, for instance f2
+    log   # this is the name of the function to run
+    shift
+    ;;
+
+  *)
+    echo "Usage: $0 {backup|log}"
+    ;;
 esac
+
